0c17fc4f70cd0d77fbee52bb0ef796563a3bab9d
mpothen9088 <124945200+mpothen9088@users.noreply.github.com>
2023-08-06 15:14:35 -0400

    Add files via upload

---
+++ backend/src/strategy/postgresql/configure.ts
@@ -0,0 +1,13 @@
+import { DataSource } from "typeorm";
+
+export const postgresDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [],
+  synchronize: true,
+  logging: false,
+});
